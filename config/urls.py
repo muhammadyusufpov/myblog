d5f4856772fff400a64e5e912ece9ca9c8b05b6a
muhammadyusufpov <muhammadyusuf.yusupov201@gmail.com>
2026-09-16 19:51:58 +0500

    chore: clean up URL configurations

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('ckeditor5/', include('django_ckeditor_5.urls')),
     path("", include("blog.urls")),
 ]
