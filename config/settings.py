e37a4ad91a6ef1eb1acf151bc4cc448bf2451a45
muhammadyusufpov <muhammadyusuf.yusupov201@gmail.com>
2026-09-16 16:06:31 +0500

    fix: update debug setting for environment variables

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9-!#=g*o&l97j@y#$s9nflk1^ge0fekuiore-&znnd0i7oh-do"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['yusupov.onrender.com', 'localhost', '127.0.0.1']
 
