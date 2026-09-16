eb1aa2b47042cfccf98ff35cdd2d676792554eb6
muhammadyusufpov <muhammadyusuf.yusupov201@gmail.com>
2026-09-16 19:47:22 +0500

    feat: improve Post display and filters

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "content_format", "created_at")
+    list_filter = ("content_format", "category")
+    readonly_fields = ("content_format",)
