bf7c0cc2d29376f051766cfc8fa80c010cd82d75
muhammadyusufpov <muhammadyusuf.yusupov201@gmail.com>
2026-09-18 08:26:19 +0500

    feat: update PostAdmin fields

--- blog/admin.py
+++ blog/admin.py
@@ -6,4 +6,5 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "content_format", "created_at")
     list_filter = ("content_format", "category")
-    readonly_fields = ("content_format",)
+    fields = ("category", "title", "content", "content_format", "created_at")
+    readonly_fields = ("created_at",)
